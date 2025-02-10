27fd03d579a67bc0a639d8f3d7821f16a2aab379
lidyaT21 <156967948+lidyaT21@users.noreply.github.com>
2025-02-10 01:00:58 +0100

    added

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import { initPage } from "./pages/initPage.js";
+
+const loadApp = () => {
+  initPage();
+};
+
+window.addEventListener("load", loadApp);
---
+++ src/constants.js
@@ -0,0 +1,38 @@
+export const constants = {
+  USER_INTERFACE_ID: "user-interface",
+  SEARCH_BUTTON_ID: "search-button",
+  SEARCH_FORM_ID: "search-form",
+  SEARCH_INPUT_ID: "search-input",
+  RESULTS_ID: "results",
+  //MOVIE_ID: "movie",
+  MOVIE_DETAILS_ID: "movie-details",
+  LOADING_ID: "loading",
+  HOME_BUTTON_ID: "home-button",
+  ERROR_ELEMENT_ID: "error-element",
+  // Classes
+  CONTAINER_CLASS: "container",
+  POPULAR_MOVIE_CLASS: "popular",
+  MOVIE_CONTAINER_CLASS: "movie-container",
+  CARD_CLASS: "card",
+  CARD_IMG_CLASS: "card-img-top",
+  CARD_BODY_CLASS: "card-body",
+  CARD_TITLE_CLASS: "card-title",
+  CARD_BODY_CLASS: "card-text",
+
+  TOP_DETAILS_CLASS: "details-top",
+  BOTTOM_DETAILS_CLASS: "details-bottom",
+  RESULT_TITLE_CLASS: "result-title",
+  RESULT_INSTRUCTIONS_CLASS: "result-instructions",
+  RESULT_IMAGE_CLASS: "result-image",
+  BUTTON_CLASS: "btn",
+  BACK_CLASS: "back",
+  HIDE_CLASS: "hide",
+  BACK_BUTTON_CLASS: "btn",
+  READ_MORE_BUTTON_CLASS: "read-more-btn",
+  SPINNER: "spinner",
+  // API
+  // Register your key at https://www.themoviedb.org/settings/api and enter here
+
+  API_KEY: "9c37f856ea60850163c210717dbe62f2",
+  API_URL: "https://api.themoviedb.org/3",
+};
